5f00fe5fd43296e7f66aef841b0804ffef928a26
Léo Jansen <leo@leonardojansen.com>
2025-09-18 23:54:11 -0300

    Update Hero section with new image and styles
    Added new PNG and SVG assets for the Hero section and updated the layout to include the new image. Improved background and button styling for better visual presentation.

--- app/sections/Hero.jsx
+++ app/sections/Hero.jsx
@@ -5,17 +5,27 @@ import Link from 'next/link';
 export default function Hero() {
   return (
     <section className="relative w-full h-screen bg-black text-white flex items-center justify-start overflow-hidden">
-      {/* Background Image/Video - Use a imagem da primeira página */}
 
-      {/* Conteúdo da Seção Hero */}
-      <div className="relative z-10 text-center w-1/2 h-full flex flex-col justify-center items-center bg-[#10151ba1] backdrop-blur-[1px] p-8 ">
-        {/* Título Principal */}
-        <h1 className="font-title text-4xl md:text-6xl font-bold mb-4">
-          Etti Project
-        </h1>
 
-        {/* Proposta de Valor */}
-        <p className="text-lg md:text-2xl mb-8">
+      
+      <div className="relative z-10 text-center w-1/2 h-full flex flex-col justify-around items-center bg-[#0f141a94] backdrop-blur-[1px] p-8 ">
+      <div className='flex flex-col items-center bg-[#0f141a94] rounded-[220px]'>
+        <Image
+          src="/assets/ettiColor.png"
+          alt="Hero Image"
+          quality={100}
+          width={683}
+          height={683}
+          className="mb-4 w-65 h-65 "
+        />
+
+      </div>
+        
+        
+
+<div className='flex flex-col items-center'>
+ {/* Proposta de Valor */}
+        <p className="text-lg md:text-2xl mb-8 text-[#b9b9b9] font-light tracking-wide">
           Soluções completas em instalações elétricas e automação
         </p>
 
@@ -23,7 +33,7 @@ export default function Hero() {
         <div className="flex flex-col sm:flex-row justify-center gap-4">
           <Link
             href="/orcamento"
-            className="bg-white text-black font-semibold py-3 px-8 rounded-full hover:bg-gray-200 transition-colors duration-300"
+            className="bg-[#cfcfd8] text-[#131313] font-semibold py-3 px-8 rounded-full hover:bg-gray-200 transition-colors duration-300"
           >
             Solicitar Orçamento
           </Link>
@@ -34,6 +44,8 @@ export default function Hero() {
             Conhecer Serviços
           </Link>
         </div>
+</div>
+       
       </div>
 
       <Image
