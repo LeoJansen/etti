34afb0a68949c4f37a6f49445c3d74f33eb089c9
Léo Jansen <leo@leonardojansen.com>
2025-09-23 01:50:30 -0300

    Update About section styles and content
    Refined styling for the About section, including color adjustments, border thickness, spacing, and heading size. Updated text classes for improved readability and consistency with the new color scheme.

--- app/sections/About.jsx
+++ app/sections/About.jsx
@@ -4,9 +4,9 @@ import Image from 'next/image';
 const About = () => {
   return (
     <section className="py-16 bg-gray-50 min-h-screen max-w-screen overflow-hidden z-120" id="about">
-      <div className="flex flex-col w-full h-full px-4 md:px-8 lg:px-16">
-          <div className='border-4 p-4 rounded-[4px] border-[#363636] mb-4 flex justify-center w-full'>
-<h2 className="text-4xl font-semibold text-[#363636] mb-2">Sobre a Etti Project</h2>
+      <div className="flex flex-col w-full h-full px-4 md:px-8 lg:px-16 text-[#313131] ">
+          <div className='border-2 p-4 rounded-[4px]  border-[#313131]  mb-16 flex justify-center w-full'>
+<h2 className="text-[30px] md:text-4xl font-semibold text-[#313131]">Sobre a Etti</h2>
           </div>
         <div className="flex flex-col md:flex-row w-full h-full justify-center items-center md:space-x-12">
         
@@ -24,12 +24,12 @@ const About = () => {
           </div>
           <div className="md:w-1/2 h-full flex flex-col justify-center items-center">
           
-            
-            <p className="text-xl text-gray-600 mb-6">Quem somos e a nossa missão</p>
-            <p className="text-gray-700 leading-relaxed mb-4 text-justify">
+
+            <p className="text-xl  mb-6">Quem somos e a nossa missão</p>
+            <p className=" leading-relaxed mb-4 text-justify">
                Etti Project é uma empresa especializada em <strong>soluções elétricas e automação</strong>. Nossa missão é transformar espaços através de tecnologia avançada e instalações seguras, oferecendo soluções inovadoras e sustentáveis que melhoram a qualidade de vida dos nossos clientes.
             </p>
-            <p className="text-gray-700 leading-relaxed text-justify">
+            <p className=" leading-relaxed text-justify">
               Com uma equipe experiente e certificada, oferecemos um serviço completo, desde o projeto inicial até a certificação final. Isso garante a qualidade e a conformidade em cada instalação que realizamos.
             </p>
           </div>
