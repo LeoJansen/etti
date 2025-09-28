8cea4abed794fc1c43b20e310efdeda0f3ddea86
Léo Jansen <leo@leonardojansen.com>
2025-09-28 02:01:11 -0300

    Update Services section layout and styles
    Adjusted padding and layout in ServiceCard and Services components for improved alignment and responsiveness. Updated heading sizes, colors, and grid spacing to enhance visual hierarchy and consistency.

--- app/sections/ServicesSection/Services.jsx
+++ app/sections/ServicesSection/Services.jsx
@@ -19,20 +19,20 @@ const Services = () => {
         className='-z-10'
 
       />
-      <div className="flex flex-col w-full h-full items-center justify-center  py-16">
-        <div className='bg-[#040404] place-self-end p-8 rounded-l-[6px]'>
-          <div className='flex gap-4 items-center'>
-            <div className='h-[7px] w-[145px] rounded-[1.5px] bg-[#4991EB]' />
-            <h3 className="text-[30px] md:text-[40px] font-semibold text-[#EB9948] tracking-tight uppercase">Nossos</h3>
+      <div className="flex flex-col w-full h-full items-center justify-end  py-16">
+        <div className='flex flex-col w-full justify-end items-end bg-[#040404] place-self-end p-8 rounded-l-[6px] '>
+          <div className='flex w-full justify-end gap-4 items-center '>
+            <div className='h-[7px] w-[70px] md:w-[145px] rounded-[1.5px] bg-[#4991EB]' />
+            <h3 className="text-[35px] md:text-[40px] font-semibold text-[#EB9948] tracking-tight uppercase">Nossos</h3>
 
           </div>
-          <h2 className="text-[48px] md:text-[70px] lg:text-[95px] tracking-[-0.08em]  font-extralight leading-14  mb-6 text-[#c2c2c2] ">
+          <h2 className="text-[68px] md:text-[70px] lg:text-[95px] tracking-[-0.08em]  font-extralight leading-14  mb-6 text-[#e4e3b2] ">
             Serviços
           </h2>
         </div>
 
 
-        <div className="w-full h-full grid grid-cols-1 md:grid-cols-4 gap-4 px-4 lg:px-0  lg:py-20 text-[#cecece]">
+        <div className="w-full h-full grid grid-cols-1 md:grid-cols-4 gap-4 px-4 lg:px-0  py-10 lg:py-20 text-[#cecece]">
           {servicesData.map((service, index) => (
             <ServiceCard
               key={index}
