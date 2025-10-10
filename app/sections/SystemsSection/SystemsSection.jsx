17709072c266bfa1f8a8bda5355ba5f23e54ffa5
Léo Jansen <leo@leonardojansen.com>
2025-10-10 19:19:20 -0300

    Update SystemsSection heading layout and styles
    Refactored the heading area to include a decorative horizontal bar and adjusted the structure for improved visual hierarchy. The container classes were also updated to better control spacing and alignment.

--- app/sections/SystemsSection/SystemsSection.jsx
+++ app/sections/SystemsSection/SystemsSection.jsx
@@ -6,10 +6,23 @@ import { systemsContent } from './SystemsContent';
 const SystemsSection = () => {
    return (
       <section className="hidden md:flex relative w-full min-h-screen items-stretch bg-white py-24">
-         <div className="mx-auto flex w-full max-w-6xl flex-col gap-12 px-10">
+         <div className=" flex w-full  flex-col gap-12 ">
             <div className="flex flex-col text-left">
-               <h3 className="systems-subheading">Sistemas de</h3>
-               <h2 className="systems-heading">Incêndio e Segurança</h2>
+               <div className='flex flex-col w-fit'>
+                  <div className='flex w-full'>
+                     
+                     <div className='h-[5px] w-full rounded-[1.5px] bg-[#EBC197]' />
+                     <h3 className="systems-subheading">Sistemas de</h3>
+
+                  </div>
+                  <div className='flex'>
+                  <h2 className="systems-heading">Incêndio e Segurança</h2>   
+                     </div>
+
+                  
+               </div>
+               
+               
                <p className="mt-6 max-w-3xl text-lg text-gray-600 dark:text-gray-400">
                   Proteção total para o seu espaço com nossos sistemas avançados de segurança e combate a
                   incêndio.
