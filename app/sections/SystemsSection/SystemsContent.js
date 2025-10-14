efc862b4c604341e7bcdeda3b0bf16153ed4777e
Léo Jansen <leo@leonardojansen.com>
2025-10-14 09:30:03 -0300

    Add image paths to systemsContent items
    Introduced an 'imagem' property to each object in the systemsContent array, providing image paths for use in the UI.

--- app/sections/SystemsSection/SystemsContent.js
+++ app/sections/SystemsSection/SystemsContent.js
@@ -1,22 +1,26 @@
 export const systemsContent = [
   {
     title: 'Deteção Precoce',
+    imagem: "/assets/systems/systemCard1.png",
     description:
       'Sistemas avançados de deteção de incêndios com sensores de última geração para alerta imediato.',
   },
   {
     title: 'Supressão Automática',
+    imagem: "/assets/systems/systemCard2.png",
     description:
       'Instalamos sistemas de extinção automática com sprinklers e gases limpos para proteção eficaz.',
   },
   {
     title: 'Monitorização Contínua',
+    imagem: "/assets/systems/systemCard3.png",     
     description:
       'Sistemas integrados de segurança com vigilância 24/7 e resposta automática a emergências.',
   },
   {
     title: 'Manutenção Preventiva',
-    description:
+    imagem: "/assets/systems/systemCard4.png",
+    description:  
       'Garantimos a confiabilidade e o pleno funcionamento dos sistemas de segurança e combate a incêndios através de serviços de manutenção preventiva, que incluem testes operacionais e inspeções periódicas, em conformidade com as normas técnicas.',
   }
 ];
