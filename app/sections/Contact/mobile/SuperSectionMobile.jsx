723fa0b1fcb5b3eba4109b80007449098eb63e9e
Léo Jansen <leo@leonardojansen.com>
2025-10-19 01:00:27 -0300

    Update highlight color in SuperSectionMobile
    Changed the highlighted text color from #ff7919 to #EB9948 in the SuperSectionMobile component for improved visual consistency.

--- app/sections/Contact/mobile/SuperSectionMobile.jsx
+++ app/sections/Contact/mobile/SuperSectionMobile.jsx
@@ -5,7 +5,7 @@ const SuperSectionMobile = () => {
     <div className="super-section-mobile h-[50vh] w-full flex flex-col justify-center items-start gap-12 px-6 ">
          <h2 className="text-[45px] leading-[50px] font-extralight text-[hsl(0,0%,70%)]  tracking-[-0.06em] ">
             Todo         <br />
-            <span className="    text-[#ff7919]  "> grande projeto</span>
+            <span className="    text-[#EB9948]  "> grande projeto</span>
             <br /> começa com uma conversa.</h2>
       </div>
   )
