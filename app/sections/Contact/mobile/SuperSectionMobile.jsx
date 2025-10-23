dbe0dfe7e70267bd9b7dbe2f5cbf34989b756f3f
Léo Jansen <leo@leonardojansen.com>
2025-10-22 22:16:09 -0300

    Update SuperSectionMobile layout and padding
    Changed the section height from 50vh to 400px, adjusted flex alignment from center to start, and replaced px-6 with p-6 for consistent padding.

--- app/sections/Contact/mobile/SuperSectionMobile.jsx
+++ app/sections/Contact/mobile/SuperSectionMobile.jsx
@@ -2,7 +2,7 @@
 
 const SuperSectionMobile = () => {
   return (
-    <div className="super-section-mobile h-[50vh] w-full flex flex-col justify-center items-start gap-12 px-6 ">
+    <div className="super-section-mobile h-[400px] w-full flex flex-col justify-start items-start gap-12 p-6 ">
          <h2 className="text-[45px] leading-[50px] font-extralight text-[hsl(0,0%,70%)]  tracking-[-0.06em] ">
             Todo         <br />
             <span className="    text-[#EB9948]  "> grande projeto</span>
