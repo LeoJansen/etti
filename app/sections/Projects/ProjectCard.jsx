768f351008651882bef473edae68c3564f1d2db4
Léo Jansen <leo@leonardojansen.com>
2025-11-04 14:19:14 -0300

    Update ProjectCard image rendering and layout
    Refactored image rendering in ProjectCard to use the 'fill' prop and increased container height from h-64 to h-80. Cleaned up imageProps usage and improved code clarity for image display.

--- app/sections/Projects/ProjectCard.jsx
+++ app/sections/Projects/ProjectCard.jsx
@@ -19,10 +19,7 @@ const ProjectCard = ({
   const imageWidth = image?.width;
   const imageHeight = image?.height;
   const imageProps = {
-    src: imageSource,
-    alt: altText,
-    className: 'rounded-t-[2px] object-cover',
-    sizes: '(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw',
+
   };
 
   if (imageWidth && imageHeight) {
@@ -36,13 +33,19 @@ const ProjectCard = ({
     <div {...props} className={combinedClassName} style={mergedStyle}>
       <div className='p-1'>
         <div>
-          <div className="relative h-64 w-full">
-            <Image {...imageProps} />
+          <div className="relative h-80 w-full">
+            <Image src={imageSource}
+              alt={altText}
+              fill
+
+
+              className='rounded-t-[2px] object-cover'
+              sizes='(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw' />
           </div>
           <div className="relative flex flex-col w-full p-6 z-40">
             <div className='flex items-center gap-4'>
               <h3 className="text-2xl font-medium text-[#777777] tracking-[-0.02em] mb-2">{title}</h3>
-              <div className='h-[4px] w-full bg-[#EB9948]'/>
+              <div className='h-[4px] w-full bg-[#EB9948]' />
             </div>
             <p className="text-[#707070] text-lg font-light text-justify">{description}</p>
           </div>
